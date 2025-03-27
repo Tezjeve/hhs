0d0646cb83157b27d43dc92b8f07bc36a7aa2bc3
Tezjeve <130067328+Tezjeve@users.noreply.github.com>
2025-03-27 10:55:32 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -2,20 +2,20 @@ namespace L_C_G_F {
 
     //% shim=TD_ID
     //% blockId=wordPicker
-    //% block="$word"
+    //% block="$Type"
     //% blockHidden=true
     //% word.fieldEditor="textdropdown"
     //% word.fieldOptions.decompileLiterals=true
     //% word.fieldOptions.values='hi,hello'
     //% word.defl='hello'
-    export function __wordPicker(word: string): string {
-        return word;
+    export function __wordPicker(Type: string): string {
+        return Type;
     }
 
-    //% block="Create New File-Name: $name Id: $id Type: $word"
+    //% block="Create New File-Name: $name Id: $id Type: $Type"
     //% blockId=New_File_Info
     //% word.shadow="wordPicker"
-    export function New_File_Info(name: string, id: number, word: string) {
+    export function New_File_Info(name: string, id: number, Type: string) {
 
         }
     }
